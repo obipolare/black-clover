48dc9ff725a13b47ad9bb2589443cf2bb97fb03f
gustavocadev <gustavocadev@gmail.com>
2023-08-01 22:23:02 -0500

    initial commit

---
+++ app/components/CardCharacter.tsx
@@ -0,0 +1,36 @@
+'use client';
+import { Card, CardBody, CardHeader, Image } from '@nextui-org/react';
+
+type Props = {
+  id: number;
+  name: string;
+  img: string;
+  birthdate: string;
+  description: string;
+};
+
+export const CardCharacter = ({
+  id,
+  name,
+  img,
+  birthdate,
+  description,
+}: Props) => {
+  return (
+    <Card>
+      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+        <Image
+          alt="Card background"
+          className="object-cover rounded-xl"
+          src={img}
+          width={270}
+        />
+      </CardHeader>
+      <CardBody className="overflow-visible py-2">
+        <h4 className="font-bold text-large">{name}</h4>
+        <p className="">{description}</p>
+        <small className="text-default-500">{birthdate}</small>
+      </CardBody>
+    </Card>
+  );
+};
---
+++ app/components/Header.tsx
@@ -0,0 +1,97 @@
+'use client';
+import { useState } from 'react';
+import { useTheme } from 'next-themes';
+import Link from 'next/link';
+import { Image } from '@nextui-org/react';
+
+const Header = () => {
+  const [show, setShow] = useState(false);
+  const { setTheme, theme } = useTheme();
+  const changeClassToggle = () => {
+    setShow(!show);
+  };
+  const menu = [
+    {
+      name: 'Home',
+      link: '/',
+      id: 1,
+    },
+    {
+      name: 'Personajes',
+      link: '/characters',
+      id: 2,
+    },
+    {
+      name: 'Magic Knights',
+      link: '/magic-knights',
+      id: 3,
+    },
+    {
+      name: 'Curiosities',
+      link: '/curiosities',
+      id: 4,
+    },
+  ];
+  return (
+    <header className="fixed top-0 z-20 w-full transition duration-200  shadow  bg-default-100 ">
+      <nav className="flex flex-col items-start justify-between w-11/12 mx-auto lg:flex-row lg:items-center ">
+        <div className="flex flex-row items-center justify-between w-full p-2 lg:w-auto">
+          <figure>
+            <Link href="/">
+              <Image
+                src="/assets/logo-black-clover.png"
+                width={180}
+                alt="black clover logo"
+              />
+            </Link>
+          </figure>
+        </div>
+
+        <ul
+          className={`lg:flex py-2 w-full lg:w-auto items-center ${
+            !show && 'hidden'
+          }`}
+        >
+          <div
+            className="px-4 py-2 cursor-pointer dark:hover:text-yellow-200"
+            onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d={`${
+                  theme === 'light'
+                    ? ' M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z'
+                    : ' M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z '
+                }`}
+              ></path>
+            </svg>
+          </div>
+
+          {menu.map(({ name, link, id }) => {
+            return (
+              <li className="flex" key={id}>
+                <Link
+                  href={link}
+                  className="flex-grow px-4 py-2 text-base font-semibold transition duration-200 rounded hover:text-green-700 dark:hover:text-green-300"
+                >
+                  {name}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
