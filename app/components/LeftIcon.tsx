b13be6c510cd86ba499ce1592404ba54659d19ba
gustavocadev <gustavocadev@gmail.com>
2023-08-02 11:09:53 -0500

    chore: add new icons

--- app/components/LeftIcon.tsx
+++ app/components/LeftIcon.tsx
@@ -8,7 +8,7 @@ const LeftIcon = () => {
       viewBox="0 0 24 24"
       strokeWidth={1.5}
       stroke="currentColor"
-      className="w-6 h-6"
+      className="w-8 h-8"
     >
       <path
         strokeLinecap="round"
