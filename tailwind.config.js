c34164fd36b8c39c08b0cdc09cf85701cc221188
gustavocadev <gustavocadev@gmail.com>
2023-08-02 11:08:35 -0500

    chore: use esm modules

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,15 @@
-const { nextui } = require('@nextui-org/react');
+import { nextui } from '@nextui-org/react';
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
+  theme: {},
   darkMode: 'class',
   plugins: [nextui()],
 };
